a3f4ead70ff8dca8dc334d8066e91c9301908ea7
Bruno Correia <bruno.h.c@hotmail.com>
2023-06-18 16:04:11 -0300

    Inicio da aplicação

---
+++ LojaWebCSharp/Data/LojaWebCSharpContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LojaWebCSharp.Models;
+
+namespace LojaWebCSharp.Data
+{
+    public class LojaWebCSharpContext : DbContext
+    {
+        public LojaWebCSharpContext (DbContextOptions<LojaWebCSharpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<LojaWebCSharp.Models.Department> Department { get; set; } = default!;
+
+        public DbSet<LojaWebCSharp.Models.Departamento>? Departamento { get; set; }
+    }
+}
