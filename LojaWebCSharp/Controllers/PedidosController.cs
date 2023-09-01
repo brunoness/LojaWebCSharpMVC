b4281d125c56a31c76c7ec8bd27fc7e8096c17e3
Bruno Correia <bruno.h.c@hotmail.com>
2023-08-31 22:17:02 -0300

    Criando as viewers de buscar pedidos

--- LojaWebCSharp/Controllers/PedidosController.cs
+++ LojaWebCSharp/Controllers/PedidosController.cs
@@ -5,5 +5,11 @@ namespace LojaWebCSharp.Controllers {
         public IActionResult Index() {
             return View();
         }
+        public IActionResult BuscaSimples() {
+            return View();
+        }
+        public IActionResult BuscaAgrupada() {
+            return View();
+        }
     }
 }
