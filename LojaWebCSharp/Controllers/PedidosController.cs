dd495d00a343c1421cc9408c0b33ce41a43e1733
Bruno Correia <bruno.h.c@hotmail.com>
2023-09-01 00:17:24 -0300

    Implementando Busca Agrupada

--- LojaWebCSharp/Controllers/PedidosController.cs
+++ LojaWebCSharp/Controllers/PedidosController.cs
@@ -1,6 +1,7 @@
 ﻿using LojaWebCSharp.Services;
 using Microsoft.AspNetCore.Mvc;
 
+
 namespace LojaWebCSharp.Controllers {
     public class PedidosController : Controller {
 
@@ -24,8 +25,17 @@ namespace LojaWebCSharp.Controllers {
             var result = await _pedidoService.FindByDateAsync(minDate, maxDate);
             return View(result);
         }
-        public IActionResult BuscaAgrupada() {
-            return View();
+        public async Task<IActionResult> BuscaAgrupada(DateTime? minDate, DateTime? maxDate) {
+            if (!minDate.HasValue) {
+                minDate = new DateTime(DateTime.Now.Year, 1, 1);
+            }
+            if (!maxDate.HasValue) {
+                maxDate = DateTime.Now;
+            }
+            ViewData["minDate"] = minDate.Value.ToString("yyyy-MM-dd");
+            ViewData["maxDate"] = maxDate.Value.ToString("yyyy-MM-dd");
+            var result = await _pedidoService.FindByDateGroupingAsync(minDate, maxDate);
+            return View(result);
         }
     }
 }
--- LojaWebCSharp/Models/Pedido.cs
+++ LojaWebCSharp/Models/Pedido.cs
@@ -1,9 +1,12 @@
 ﻿using LojaWebCSharp.Models.Enums;
+using System.ComponentModel.DataAnnotations;
 
 namespace LojaWebCSharp.Models {
     public class Pedido {
         public int Id { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Data { get; set; }
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Total { get; set;}
         public StatusPedido Status { get; set; }
         public Vendedor Vendedor { get; set; }
